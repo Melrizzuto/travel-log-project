1b3b615e41bb907b17a8b105e3b257b06ed52a85
Melania Rizzuto <melrizzuto4@gmail.com>
2025-07-17 11:11:53 +0200

    :sparkles: Initial project setup with ESLint, lint-staged, GitHub Actions and pnpm

---
+++ eslint.config.mjs
@@ -0,0 +1,36 @@
+import antfu from "@antfu/eslint-config";
+
+// Run this command to generate base config and vs code settings:
+// pnpm dlx @antfu/eslint-config@latest
+// @ts-check
+
+export default antfu({
+  type: "app",
+  vue: true,
+  typescript: true,
+  formatters: true,
+  ignores: [
+    ".pnpm-store",
+  ],
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+}, {
+  rules: {
+    "ts/no-redeclare": "off",
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
+});
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+  modules: ["@nuxt/eslint"],
+  eslint: {
+    config: {
+      standalone: false, // <---
+    },
+  },
+});
