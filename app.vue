3e4bb2c00cb54af2d6558a8134f98946db40d3ec
Melania Rizzuto <melrizzuto4@gmail.com>
2025-07-18 11:03:57 +0200

    :sparkles: setup tailwind (#50)

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <h1 class="text-3xl font-bold underline">Hello World!</h1>
+  <button class="btn btn-secondary">
+    View Map <Icon name="material-symbols:map-outline-rounded" size="25" />
+  </button>
 </template>
