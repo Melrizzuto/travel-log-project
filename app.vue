72648ecce42a78317e07b06dcabdb4e648afdacd
Melania Rizzuto <melrizzuto4@gmail.com>
2025-07-18 11:28:44 +0200

    Tl 11 (#51)
    * :sparkles: setup tailwind
    
    * :sparkles: add landing page

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
+<!-- <script setup lang="ts">
+import { NuxtLayout } from '#components';
+
+</script> -->
+
 <template>
-  <h1 class="text-3xl font-bold underline">Hello World!</h1>
-  <button class="btn btn-secondary">
-    View Map <Icon name="material-symbols:map-outline-rounded" size="25" />
-  </button>
+  <NuxtLayout> <NuxtPage /> </NuxtLayout>
 </template>
---
+++ components/app/nav-bar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="navbar bg-neutral text-neutral-content">
+    <div class="navbar-start">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">Travel Log</NuxtLink>
+    </div>
+    <div class="navbar-end">
+      <a class="btn">Sign in</a>
+    </div>
+  </div>
+</template>
