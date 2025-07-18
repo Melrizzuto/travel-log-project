f28dcd35bfb355b9ded7367a289a88ab9566d21a
Melania Rizzuto <melrizzuto4@gmail.com>
2025-07-18 12:06:43 +0200

    sparkles: add theme toggle (#52)

---
+++ components/app/theme-toggle.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+const colorMode = useColorMode();
+const IsDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate mx-4">
+    <!-- this hidden checkbox controls the state -->
+    <input v-model="IsDark" type="checkbox">
+
+    <!-- sun icon -->
+    <Icon name="line-md:sunny-loop" class="swap-on" size="25" />
+
+    <!-- moon icon -->
+    <Icon name="line-md:moon-loop" class="swap-off" size="25" />
+  </label>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Hello there</h1>
+        <h1 class="text-5xl font-bold">
+          Hello there
+        </h1>
         <p class="py-6">
           Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
           excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
